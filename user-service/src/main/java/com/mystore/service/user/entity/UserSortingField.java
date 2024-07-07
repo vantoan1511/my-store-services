57557757328d2c6faa9b0790560c3655353e3501
Toan Nguyen <vantoan1517@gmail.com>
2024-07-07 18:43:52 +0700

    Improve the code

--- user-service/src/main/java/com/mystore/service/user/entity/UserSortingField.java
+++ user-service/src/main/java/com/mystore/service/user/entity/UserSortingField.java
@@ -1,42 +1,23 @@
 package com.mystore.service.user.entity;
 
-import com.mystore.service.user.entity.UserInfo;
-
 import java.util.Comparator;
 
 public enum UserSortingField implements Comparator<UserInfo> {
 
-    FIRST_NAME,
-    LAST_NAME {
-        @Override
-        public int compare(UserInfo thisUser, UserInfo thatUser) {
-            return thisUser.getLastName().compareTo(thatUser.getLastName());
-        }
-    },
-    EMAIL {
-        @Override
-        public int compare(UserInfo thisUser, UserInfo thatUser) {
-            return thisUser.getEmail().compareTo(thatUser.getEmail());
-        }
-    },
-    USERNAME {
-        @Override
-        public int compare(UserInfo thisUser, UserInfo thatUser) {
-            return thisUser.getUsername().compareTo(thatUser.getUsername());
-        }
-    },
-    GENDER {
-        @Override
-        public int compare(UserInfo thisUser, UserInfo thatUser) {
-            return thisUser.getGender().name().compareTo(thatUser.getGender().name());
-        }
-    };
+    FIRST_NAME(Comparator.comparing(UserInfo::getFirstName)),
+    LAST_NAME(Comparator.comparing(UserInfo::getLastName)),
+    EMAIL(Comparator.comparing(UserInfo::getEmail)),
+    USERNAME(Comparator.comparing(UserInfo::getUsername)),
+    GENDER(Comparator.comparing(user -> user.getGender().name()));
 
-    UserSortingField() {
+    private final Comparator<UserInfo> comparator;
+
+    UserSortingField(Comparator<UserInfo> comparator) {
+        this.comparator = comparator;
     }
 
     @Override
     public int compare(UserInfo thisUser, UserInfo thatUser) {
-        return thisUser.compareTo(thatUser);
+        return comparator.compare(thisUser, thatUser);
     }
 }
