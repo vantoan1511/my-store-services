b5da2d9674d166957cb90e74e5332150f25c011c
Toan Nguyen <vantoan1517@gmail.com>
2024-08-10 16:18:24 +0700

    Improve sort field

--- user-service/src/main/java/com/mystore/user/UserSort.java
+++ user-service/src/main/java/com/mystore/user/UserSort.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserSort {
 
-    @QueryParam("sortBy")
+    @QueryParam("sort_by")
     private UserSortField sortField = UserSortField.DEFAULT;
 
     @QueryParam("descending")
--- user-service/src/main/java/com/mystore/user/UserSortField.java
+++ user-service/src/main/java/com/mystore/user/UserSortField.java
@@ -7,6 +7,14 @@ import java.util.Comparator;
 @Getter
 public enum UserSortField {
 
+    FIRST_NAME(Comparator.comparing(User::getFirstName, Comparator.nullsFirst(Comparator.naturalOrder()))),
+
+    LAST_NAME(Comparator.comparing(User::getLastName, Comparator.nullsFirst(Comparator.naturalOrder()))),
+
+    USERNAME(Comparator.comparing(User::getUsername, Comparator.nullsFirst(Comparator.naturalOrder()))),
+
+    EMAIL(Comparator.comparing(User::getEmail, Comparator.nullsFirst(Comparator.naturalOrder()))),
+
     CREATED_AT(Comparator.comparing(User::getCreatedAt, Comparator.nullsFirst(Comparator.naturalOrder()))),
 
     DEFAULT(Comparator.comparing(User::getCreatedAt, Comparator.nullsFirst(Comparator.naturalOrder()))
